191b636b7ecb7dbda4cc19d4c86396bc2413ad1a
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-04-03 22:29:35 -0300

    atualizações

--- js/main.js
+++ js/main.js
@@ -101,34 +101,6 @@
     offset: '80%'
   });
 
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
   // Initi AOS
   AOS.init({
