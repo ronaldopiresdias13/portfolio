6626dcd75738e4210159b98866d18921e87ad5b2
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-04-04 00:48:05 -0300

    malhorias

---
+++ projetos/cartao-credito/js/cep.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("input[name=cep]").blur(function () {
+        var cep = $(this).val().replace(/[^0-9]/, '');
+        if (cep) {
+            var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+            $.ajax({
+                url: url,
+                dataType: 'jsonp',
+                crossDomain: true,
+                contentType: "application/json",
+                success: function (json) {
+                    $("input[name=cidade]").val(json.localidade);
+                    $("input[name=uf]").val(json.uf);
+                }
+            });
+        }
+    });
+});
